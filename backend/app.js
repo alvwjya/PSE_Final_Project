c05b5e69811c5218244605ee211c26d65696bf1e
davinpchandra <57931602+davinpchandra@users.noreply.github.com>
2022-01-03 15:40:06 +0700

    Connect to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
-//psedatabase
+const port = process.env.PORT || 4000;
+const {MONGOURI} = require('./keys');
+
 //Initialize
 dotenv.config();
 const app = express();
 
+mongoose.connect(MONGOURI, {
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
+mongoose.connection.on('connected', ()=> {
+    console.log('Connected to MongoDB');
+});
+mongoose.connection.on('error', (err)=> {
+    console.log('Connection error', err);
+});
+
 const mqttClient = require('./mqtt');
 
 //Middleware
+app.use(express.json());
+
+//Routes
+
+
+//Port
+app.listen(port, ()=> {
+    console.log('Server is running on port: ', port);
+});
