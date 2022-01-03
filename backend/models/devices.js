07b6ec28e6169adf8d813dc56a453d622c40b96c
davinpchandra <57931602+davinpchandra@users.noreply.github.com>
2022-01-03 21:39:13 +0700

    Create devices.js

---
+++ backend/models/devices.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const deviceSchema = new mongoose.Schema({
+    deviceName: {
+        type: String,
+        required: true
+    },
+    id: {
+        type: Number,
+        required: true
+    },
+    //Purpose is for "ac" or "lamp"
+    purpose: {
+        type: String,
+        required: true
+    }
+});
+
+mongoose.model("Devices", deviceSchema);
