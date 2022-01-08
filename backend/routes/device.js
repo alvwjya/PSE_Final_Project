34048c169073b8cbb1a34ccae8e2524930cd9d41
davinpchandra <57931602+davinpchandra@users.noreply.github.com>
2022-01-08 19:04:29 +0700

    Update routes

--- backend/routes/device.js
+++ backend/routes/device.js
@@ -2,13 +2,30 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const Devices = mongoose.model('Devices');
-import {getDevices, addDevice} from '../controllers/deviceController';
+require('../models/devices');
 
+const addDevice = async (req, res) => {
+    const device = new Devices(req.body);
+    try {
+        const addedDevice = await device.save();
+        res.status(201).json(addedDevice);
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+}
 
+const getDevices = async (req, res) => {
+    try {
+        const devices = await Devices.find();
+        res.json(devices);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
 
 //Get all devices
 router.get('/', getDevices);
 //Add device
 router.post('/', addDevice);
 
-export default router;
+module.exports = router;
--- backend/routes/room.js
+++ backend/routes/room.js
@@ -1,7 +1,28 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-import {getRooms, addRoom} from '../controllers/roomController';
+require('../models/rooms');
+
+const addRoom = async (req, res) => {
+    const room = new Rooms(req.body);
+    try {
+        const addedRoom = await room.save();
+        res.status(201).json(addedRoom);
+    } catch (error) {
+        res.status(400).json({message: error.message});
+    }
+}
+
+const getRooms = async (req, res) => {
+    try {
+        const rooms = await Rooms.find();
+        res.json(rooms);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
 
 router.get('/', getRooms);
 router.post('/', addRoom);
+
+module.exports = router;
