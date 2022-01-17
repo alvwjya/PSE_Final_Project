db078f5129a16b74b532910d711a67389e7e7535
christynatalia <57931555+christynatalia@users.noreply.github.com>
2022-01-17 22:04:17 +0700

    it will show the current condition of the room

--- frontend/src/components/SensorsGalery.js
+++ frontend/src/components/SensorsGalery.js
@@ -5,20 +5,60 @@ import './SensorsGalery.css';
 import { Link } from 'react-router-dom';
 import axios from "axios";
 
+const apiAC = axios.create({
+    baseURL: 'http://localhost:4000/ac'
+})
+
+const apiLamp = axios.create({
+    baseURL: 'http://localhost:4000/Lamp'
+})
 
 
 const SensorsGalery = () => {
-    const [data, setData] = useState([])
-    useEffect(() => {
-        axios.get('http://localhost:4000/ac')
-            .then(response => {
-                setData(response.data)
-                console.log(data)
-            })
-            .catch(error => {
-                console.log("Error getting the data", error);
-            })
-    })
+
+    const [RoomTemp, setRoomTemp] = useState("")
+    const [RoomHumidity, setRoomHumidity] = useState("")
+    const [RoomBrightness, setRoomBrightness] = useState("")
+    const [RoomMotion, setRoomMotion] = useState("")
+
+
+    apiAC.get('/').then(
+        (response) => {
+            //console.log(response)
+            setRoomTemp(response.data["Room Temp"])
+            setRoomHumidity(response.data["Room Humidity"])
+        }
+    )
+
+    apiLamp.get('/').then(
+        (response) => {
+            //console.log(response)
+            setRoomBrightness(response.data["Room Brightness"])
+            setRoomMotion(response.data["Room Motion"])
+        }
+    )
+
+    const setBrightValue = () => {
+        if (RoomBrightness == 0) {
+            return "Dark"
+        }
+        else {
+            return "Light"
+        }
+    }
+
+    const setMotionValue = () => {
+        if (RoomMotion == 0) {
+            return "No movement"
+        }
+        else {
+            return "There is a movement"
+        }
+    }
+
+
+
+
     return (
         <>
             <Navbar />
@@ -27,19 +67,18 @@ const SensorsGalery = () => {
                 <div className="title-header">
                     <h2 className="text-start header ml-3 p-2">Sensors:</h2>
                 </div>
-                <div className="container-room-description ml-3 p-2">
-                    <p className="text p-0">Room Temperature: </p>
-                    <p className="text p-0">Humidity: x</p>
-                </div>
+
 
                 <div className="row">
 
 
-                    <div className='col-md-4'>
+                    <div className='col-md-6'>
                         <div className="card ml-3 p-2">
                             <div className="card-body">
-                                <h5 className="card-title">Lamp</h5>
-                                <p className="card-text mx-0 px-0">This is Lamp</p>
+                                <h5 className="card-title">AC</h5>
+                                <p className="card-text mx-0 px-0">This is AC</p>
+                                <p className="text p-0">Room Temperature: {RoomTemp} </p>
+                                <p className="text p-0">Room Humidity: {RoomHumidity}</p>
                                 <p className="card-text mx-0 px-0">Type: AC</p>
                                 <div className="container-button">
                                     <a href="#" className="btn btn-primary turn-button">Turn on</a>
@@ -65,11 +104,13 @@ const SensorsGalery = () => {
 
 
 
-                    <div className='col-md-4'>
+                    <div className='col-md-6'>
                         <div className="card ml-3 p-2">
                             <div className="card-body">
                                 <h5 className="card-title">Sensors Name</h5>
                                 <p className="card-text mx-0 px-0">Sensors Description</p>
+                                <p className="text p-0">Room Brightness: {setBrightValue()} </p>
+                                <p className="text p-0">Room Motion: {setMotionValue()}</p>
                                 <p className="card-text mx-0 px-0">Type: Lamp</p>
                                 <div className="container-button">
                                     <a href="#" className="btn btn-primary turn-button">Turn on</a>
